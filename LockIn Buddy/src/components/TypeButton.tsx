bac4f39d0a6adbd2262ffefe1796839208c97844
Katherina Dayaon <katherinadayaon@gmail.com>
2026-03-25 21:57:33 -0400

    added different modes

--- LockIn Buddy/src/components/TypeButton.tsx
+++ LockIn Buddy/src/components/TypeButton.tsx
@@ -1,9 +1,28 @@
-function TypeButton({ text, onClick }: { text: string; onClick: () => void }) {
+export type ButtonMode = "lockIn" | "shortBreak" | "longBreak";
+
+function TypeButton({
+  text,
+  mode,
+  activeMode,
+  onClick,
+}: {
+  text: string;
+  mode: ButtonMode;
+  activeMode: ButtonMode;
+  onClick: () => void;
+}) {
+  const isActive = mode === activeMode;
+
   return (
     <button
       type="button"
       onClick={onClick}
-      className="cursor-pointer rounded-xl bg-[var(--classicWhite)] px-[1em] py-[0.25em] text-[1.3em] text-[var(--customGreen)]"
+      className={[
+        "cursor-pointer rounded-xl px-[1em] py-[0.25em] text-[1.3em] transition",
+        isActive
+          ? "bg-[var(--customGreen)] text-[var(--classicWhite)]"
+          : "bg-hidden text-[var(--customGreen)]",
+      ].join(" ")}
     >
       {text}
     </button>
