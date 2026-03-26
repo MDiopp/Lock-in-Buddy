82297f0b924e4f1a00fb0fc41d0a9cc93b393dcf
Katherina Dayaon <katherinadayaon@gmail.com>
2026-03-25 20:08:51 -0400

    started load in page

--- LockIn Buddy/src/App.tsx
+++ LockIn Buddy/src/App.tsx
@@ -5,8 +5,10 @@ function App() {
 
 
   return (
-    <main className="container">
-      <h1 className="text-red-500">Welcome to Tauri + React</h1>
+    <main className="flex justify-center items-center h-screen bg-[var(--customGreen)] flex-col">
+      <h2 className="text-4xl text-[var(--lighterGreen)]">Welcome to</h2>
+      <h1 className="text-5xl text-[var(--classicWhite)]">LockIn Buddy!</h1>
+      <p className="text-xl mt-5 text-[var(--lighterGreen)]">lock in with the help of a 'friendly' face...</p>
     </main>
   );
 }
